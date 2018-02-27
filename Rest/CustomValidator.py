eb1f33bb3059f0f88d5e94a455c5c5c315099ae5
Erick Maia da Silva <erick.silva@CONTMATIC.AD>
2018-02-27 17:50:32 -0300

    teste

---
+++ Rest/CustomValidator.py
@@ -0,0 +1,33 @@
+
+# coding: utf-8
+
+# In[2]:
+
+class GenericDescriptor:
+    def __init__(self, getter, setter):
+        self.getter = getter
+        self.setter = setter
+
+    def __get__(self, instance, owner=None):
+        if instance is None:
+            return self
+        return self.getter(instance)
+
+    def __set__(self, instance, value):
+        return self.setter(instance, value)
+
+
+# In[ ]:
+
+def NotNull(attr_name):
+    def decorator(cls):
+        name = "__" + attr_name
+        def getter(self):
+            return getattr(self, name)
+        def setter(self, value):
+            assert value is not None, ("O atributo: " + attr_name + " Deve ser informado.")
+            setattr(self, name, value)
+        setattr(cls, attr_name, GenericDescriptor(getter, setter))
+        return cls
+    return decorator
+
