32591ac9e4b08a4dde07fdc21bd6b3b955d0ae16
Erick Maia da Silva <erick.silva@CONTMATIC.AD>
2018-03-01 17:50:28 -0300

    teste4

--- Rest/controller.py
+++ Rest/controller.py
@@ -17,15 +17,16 @@ class HelloWorld(Resource):
 
     def get(self):
         return jsonify({"data":"ok"})
-    
+
     def post(self):
         nome = request.json['nome']
         try:
             usuario = Usuario(nome=None)
         except ResourceBussinessException as exception:
             return self.tratarResourceBussinessException(exception)
-            
+
 api.add_resource(HelloWorld, '/hello')
 
 if __name__ == '__main__':
     app.run(port=5002)
+
--- Rest/model.py
+++ Rest/model.py
@@ -1,6 +1,5 @@
-
 from validator import NotNull
-from error_handling import ResourceBussinessException
+
 @NotNull(attr_name="nome")
 @NotNull(attr_name="idade")
 class Usuario():
